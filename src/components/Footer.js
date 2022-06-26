9023877a671faa2c5ea20f49490d1e86a1bf0312
Den <d3n.vor@gmail.com>
2022-06-26 23:15:33 +0300

    fixed some bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ function Footer() {
 				<div className="footer-copyright">
 					<div className="container">
 					© {new Date().getFullYear()} Copyright Text
-					<a className="grey-text text-lighten-4 right" href="#">Repo</a>
+					<a 
+					className="grey-text text-lighten-4 right" 
+					href="https://github.com/NumbDV/fortnite-shop" 
+					target='_blank' 
+					rel="noreferrer">Repo</a>
 					</div>
 				</div>
 			</footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ function Header() {
 
 	return <nav className="blue darken-3">
 				<div className="nav-wrapper">
-				<a href="#" className="brand-logo">Fortnite shop</a>
+				<a href="/" className="brand-logo">Fortnite shop</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li><a href="#">Repo</a></li>
+					<li><a href="https://github.com/NumbDV/fortnite-shop" target='_blank' rel="noreferrer">Repo</a></li>
 				</ul>
 				</div>
 			</nav>
